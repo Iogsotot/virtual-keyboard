0a2c861f2410cd6163ffbc16e074a59cf90d7265
Iogsotot <ajustusa@gmail.com>
2020-10-30 12:32:34 +0300

    feat: add Key.js & updte scripts

---
+++ src/scripts/Key.js
@@ -0,0 +1,22 @@
+// eslint-disable import/extensions
+import create from './utils/create.js';
+
+export default class Key {
+  constructor({ small, shift, code }) {
+    this.code = code;
+    this.small = small;
+    this.shift = shift;
+    this.isFnKey = Boolean(small.match(/Ctrl|arr|Alt|Shift|Tab|Back|Del|Enter|Caps|Esc\Win/));
+
+    // проверяем, что если в объекте по ключу shift лежит не буквы и не цифры, то 
+    // создаём элемент div с классом sub и записывает в свойства экземпляра класса Key
+    if(shift && shift.match(/[^a-zA-Zа-ЯА-яёЁ0-9]/)) {
+      this.sub = create('div', 'sub', this.shift);
+    } else {
+      this.sub = create('div', 'sub', '');
+    }
+
+    this.letter = create('div', 'letter', small);
+    this.div = create('div', 'keyboard__key', [this.sub, this.letter], null, ['code', this.code]);
+  }
+}
--- src/scripts/storage.js
+++ src/scripts/storage.js
@@ -3,11 +3,11 @@ export function set(name, value) {
 }
 
 export function get(name, subst = null) {
-  return JSON.parse(window.localStorage.getItem(name || subst));
+  return JSON.parse(window.localStorage.getItem(name || subst));      //subst - подстраховка
 }
 
-// export function del(name, subst = null) {
-//   return window.localStorage.removeItem(name) | subst;
-// }
+export function del(name, subst = null) {
+  return window.localStorage.removeItem(name || subst);
+}
 
 // lang = get('kbLang', '"ru"');
